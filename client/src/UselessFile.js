3c6f35dd3cc8750baa2dcd280a4ad9bc370e869a
zyzzyxis <13ezimm@gmail.com>
2021-06-11 12:56:15 -0600

    installed reactrouter dom, now working

--- client/src/UselessFile.js
+++ client/src/UselessFile.js
@@ -1,6 +1,6 @@
 // Hey Yo
 // figure out routes
-// check over model and controller 
+// TODO check over model and controller 
 // homepage to display data and determine what we want this to look like 
 // rails axios calls, figure out postman because I dunno 
 // seeds file
