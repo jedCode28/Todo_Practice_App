72bc7f4276ef9d04f858782dfcd94c7da954a4ef
zyzzyxis <13ezimm@gmail.com>
2021-06-11 12:58:13 -0600

    project list for tomorrow

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,6 +4,8 @@ const Home = () => {
   return (
     <div>
       this is home
+      for home we need to make functions for show first (need to create seeds file)
+      then destroy, then new, then update. yay
     </div>
   )
 }
